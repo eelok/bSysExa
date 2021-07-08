64fbd528045a4e8d51ca2b38d9fcfb804cb91bc5
eelok <eelok@mail.ru>
2021-07-08 09:02:11 +0200

    improve file childrenCommunication.c

--- childrenCommunication.c
+++ childrenCommunication.c
@@ -5,52 +5,46 @@
 #include <fcntl.h>
 
 int main(int argc, char *argv[]){
-    int parent_id;
     int firstChild_id, secondChild_id;
     int fd[2];
     int bufferSize = 1024;
 
-    parent_id = getpid();
-
     if(argc < 2){
-        printf("Insert 2 command line arguments");
+        printf("Error. Please insert 2 command line arguments\n");
         exit(1);
     }
 
-    if(pipe(fd) != 0){
+    if(pipe(fd) == -1){
         printf("Error initialising pipe!\n");
         exit(1);
     }
 
     if ((firstChild_id = fork()) == 0) { //First Child
-        firstChild_id = getpid();
         char buffer[bufferSize];
-        printf("First Child PID: %d, its Parent ID: %d\n", firstChild_id, parent_id);
-
+        
         int fileDescriptor = open(argv[1], O_RDONLY);
         if(fileDescriptor < 0){
-            printf("Error open a file\n");
-            exit(2);
+            printf("Failed to open the file\n");
+            exit(1);
         }
       
         int numOfBytes = read(fileDescriptor, buffer, bufferSize);//reads from file
         write(fd[1], buffer, numOfBytes);//writes to pipe
 
-        printf("First Child read from file: %s\n", buffer);
         close(fd[0]);
     } else { //Parent Process
         if((secondChild_id = fork())== 0){ //Second Child
-            secondChild_id = getpid();
             char buff[bufferSize];
-            printf("Second Child PID: %d, its parent: %d\n", secondChild_id, getppid());
 
             int fileDescriptor = open("outputfile.txt", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IXUSR);
-            int nread = read(fd[0], buff, bufferSize);
-            write(fileDescriptor, buff, nread);
+            if(fileDescriptor == -1){
+                printf("Failed to open the file\n");
+                exit(1);
+            }
+
+            int numOfBytes = read(fd[0], buff, bufferSize);
+            write(fileDescriptor, buff, numOfBytes);
             close(fd[1]);
-            
-        } else {//Parent Process
-            printf("Parent ID %d\n", getpid());
         }
     }
     return 0;
